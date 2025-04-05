4b5366e8400a706539fb1c158fb29f1b9001dc6e
yashraj63929 <yashrajshukla90@gmail.com>
2025-04-05 09:51:15 +0530

    first commit

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+module.exports.authUser = async(req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id)
+
+        req.user = user;
+
+        return next();
+
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
